eff65b1f8677709422b0b3951e169bf493c5edd7
Jon Toews <jtoews@Jons-iMac.hsd1.ca.comcast.net>
2015-08-24 18:47:13 -0700

    Initial annotations

---
+++ src/main/java/org/su/SimplistUnit/App.java
@@ -0,0 +1,13 @@
+package org.su.SimplistUnit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
